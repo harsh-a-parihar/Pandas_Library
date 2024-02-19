140e60de20c24bd5a97f71ca80fd036cfe4e4379
abcd <nextmachenic@gmail.com>
2024-02-19 12:51:55 +0530

    added_few_methods_to_DataFrames.py

--- 3_DataFrames.py
+++ 3_DataFrames.py
@@ -16,6 +16,22 @@ print('\n', df1.shape)      # prints shape of dataframe
 print('\n', df1.describe())     # prints full arithmatic opps of dataframe
 print('\n', df1.dtypes)     # prints all the dtype of elements
 print('\n', df1.dtypes.value_counts())      # prints total number of values
+print('\n', df1.sort_index(ascending=False))    # sorts index in descending order
+print('\n', df1.sort_values('Marks', ascending=False))  # sorts value given in descending order
+
+# group data and calculate sum(), mean(), count(), etc.
+# dataframe.groupby('Column_name_you_want_to_group')['column1', 'column2', 'column3'].sum()/.mean()/etc.
+
+# time_series (dtype: datetime)
+# pd.to_datetime(series_of_dataframe)
+# pd.DatetimeIndex(series_of_dataframe).year/.month/.day/.weekday
+
+# merge two dataframes(data1 and data2) using one common column
+# merged_df = data1.merge(data2, on='common_column_name')
+
+# save_result in csv file
+# 1st: result_df = merged_df[['column1', 'column2', 'column3', ...]]
+# 2nd: result_df.to_csv('result.csv', index=False)      # file saved
 
 print('\n')     # gap
 
@@ -34,6 +50,11 @@ df2 = pd.DataFrame({
 print(df2.loc['India'])
 print(df2.iloc[2])
 print(df2['Population'])
+
+print('\n')     # gap
+
+# extracting data from the dataframe
+# datafram.at[index, 'column_name']
 
 print('\n')     # gap
 
